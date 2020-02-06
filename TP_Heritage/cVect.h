f82c2775fb25f90901de3459cbcdc40b7a7e7e71
RMocky <r.mock@ludus-academie.com>
2020-02-06 11:48:25 +0100

    1er exercice commenté

--- TP_Heritage/cVect.h
+++ TP_Heritage/cVect.h
@@ -3,13 +3,13 @@ class cVect
 {
 protected:
 
-	int nElem;
-	int *adr;
+	int nElem;	//Nombre d'éléments
+	int *adr;	//Zone mémoire contenant les éléments
 
 public:
 
-	cVect(int);
-	~cVect();
-	int & operator [] (int);
+	cVect(int);	//Constructeur
+	~cVect();	//Destructeur
+	int & operator [] (int);	//Surchage d'opérateur []
 };
 
--- TP_Heritage/cVectb.h
+++ TP_Heritage/cVectb.h
@@ -5,18 +5,18 @@ class cVectb : cVect
 {
 private:
 
-	int nMin;
-	int nMax;
+	int nMin;	//Borne minimale
+	int nMax;	//Borne maximale
 
 public:
 
-	cVectb(int nDebut, int nFin) : cVect(nFin - nDebut + 1)
+	cVectb(int nDebut, int nFin) : cVect(nFin - nDebut + 1)	//Constructeur
 	{
 		nMin = nDebut; 
 		nMax = nFin;
 	}
 
-	int & operator [] (int index)
+	int & operator [] (int index)	//Surchage
 	{
 		return adr[index - nMin];
 	}
